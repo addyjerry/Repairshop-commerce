0c02ab635450afc1b59f73e7f0074e1f8f1a630f
addyjerry <addyjerry2704@gmail.com>
2025-07-29 17:25:21 +0000

    m

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -8,7 +8,7 @@ export default function page() {
   return (
     <div className="py-50">
       <div className="flex flex-col md:flex-row justify-between">
-        <h1 className="text-3xl font-bold text-center w-3/4">Our products</h1>
+        <h1 className="text-3xl font-bold text-center">Our products</h1>
       </div>
       <div className="flex-row">
         <NewProducts />
