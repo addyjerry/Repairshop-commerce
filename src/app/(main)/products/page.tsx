64f70d9d7a6518e29d0cd5b8ecefd61f266dadb0
addyjerry <addyjerry2704@gmail.com>
2025-07-29 17:19:25 +0000

    more carousel

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -1,7 +1,6 @@
 import FanProducts from "@/components/FanProducts";
 import IronProducts from "@/components/IronProducts";
 import NewProducts from "@/components/NewProducts";
-import Searchbar from "@/components/Searchbar";
 import SpeakerProducts from "@/components/SpeakerProducts";
 import TvProducts from "@/components/TvProducts";
 
@@ -10,7 +9,6 @@ export default function page() {
     <div className="py-50">
       <div className="flex flex-col md:flex-row justify-between">
         <h1 className="text-3xl font-bold text-center w-3/4">Our products</h1>
-        <Searchbar />
       </div>
       <div className="flex-row">
         <NewProducts />
